9a786a1858bd68c0d2d631d9fca382332fa92cdd
Ragshahini Navarathna <151517968+Ragshahini@users.noreply.github.com>
2025-01-10 22:41:53 +0530

    pic

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/', // This is crucial for GitHub Pages
+  base: '/Portfolio/', // Required for proper asset resolution on GitHub Pages
   plugins: [react()],
   css: {
     modules: {
-      localsConvention: "camelCase", // Ensure camelCase naming for CSS module classes
+      localsConvention: 'camelCase', // Use camelCase for CSS module classes
     },
   },
 });
