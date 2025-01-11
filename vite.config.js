0fdacc2cbb374358c7b31fb8da91f0a3ac188eef
Ragshahini Navarathna <151517968+Ragshahini@users.noreply.github.com>
2025-01-11 11:33:57 +0530

    edited the package

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/', // Required for proper asset resolution on GitHub Pages
   plugins: [react()],
   css: {
     modules: {
